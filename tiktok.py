8c61c81499460341d2081d1b99d539e380b982ff
halohack <79331796+halocrak@users.noreply.github.com>
2021-11-05 16:09:14 +0300

    Add files via upload

---
+++ tiktok.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import time
+import requests
+os.system('clear')
+
+
+
+logo = """   \033[5;37;40m
+
+-------------------------------
+      AUTHER: HALO
+       [+] Instagram : i4m_rio
+       [+] Telegram  : i4m_mrx
+       [+] snapchat  : im_halo0
+       [+] github    : halocrak
+       [+] facebook  : imfor halo
+       [+] youtube   : MRX CODAR
+-------------------------------
+"""
+
+print logo
+print '\033[5;30;40m1- HACK VIEW TIK TOK'
+print '2- CHECKER USER TIKTOK'
+
+im = raw_input('HALLBZHERA:')
+if im == '1':
+    import os
+    os.system("rm -rf hm")
+    os.system("git clone https://github.com/halocrak/hm")    
+    os.system("python2 hm/hm.py")
+if im == '2':
+    import os
+    os.system("rm -rf chk")
+    os.system("git clone https://github.com/halocrak/chk")    
+    os.system("python chk/CHE.py") 
+    sys.exit()
