af316678f0250c721b27409b6471cc5e70b26e0e
halohack <79331796+halocrak@users.noreply.github.com>
2021-11-05 15:56:03 +0300

    Add files via upload

---
+++ insta.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import time
+import requests
+os.system('clear')
+
+
+
+logo = """   \033[5;37;40m
+
+-------------------------------
+      AUTHER: HALO
+       [+] Instagram : i4m_rio
+       [+] Telegram  : i4m_mrx
+       [+] snapchat  : im_halo0
+       [+] github    : halocrak
+       [+] facebook  : imfor halo
+       [+] youtube   : MRX CODAR
+-------------------------------
+"""
+
+print logo
+print '\033[1;31;40m1- REPORT INSTA U TIKTOK'
+print '2- CRACK INSTAGRAM[NEW]'
+print '3- AUTO FOLO INSTAGRAM'
+print '4- CHECKER USER INSTA'
+print'5-INFORMATION INSTA'
+
+im = raw_input('HALLBZHERA:')
+if im == '1':
+    import os
+    os.system("clear")
+    os.system("git clone https://github.com/halocrak/staf")    
+    os.system("python staf/stafwa7sh.py")
+if im == '2':
+    import os
+    os.system("clear")
+    os.system("rm -rf all")
+    os.system("git clone https://github.com/halocrak/all.git")    
+    os.system("python all/qw.py")
+if im == '3':
+    import os
+    os.system("clear")
+    os.system("rm -rf all")
+    os.system("git clone https://github.com/halocrak/all.git")    
+    os.system("python all/ot.py")
+if im == '4':
+    import os
+    os.system("clear")
+    os.system("rm -rf ck1")
+    os.system("git clone https://github.com/halocrak/ck1")    
+    os.system("python3 ck1/ck.py")
+if im == '5':
+    import os
+    os.system("clear")
+    os.system("rm -rf all")
+    os.system("git clone https://github.com/halocrak/all")    
+    os.system("python3 all/if.py")
+    sys.exit()
