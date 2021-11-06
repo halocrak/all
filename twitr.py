1e3a444dacd1507ae7c115d74df000dcf2172941
halohack <79331796+halocrak@users.noreply.github.com>
2021-11-06 12:20:08 +0300

    Add files via upload

---
+++ twitr.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import time
+import requests
+os.system('clear')
+
+
+
+logo = """   \033[5;37;40m
+
+-------------------------------
+      AUTHER: HALO
+       [+] Instagram : i4m_rio
+       [+] Telegram  : i4m_mrx
+       [+] snapchat  : im_halo0
+       [+] github    : halocrak
+       [+] facebook  : imfor halo
+       [+] youtube   : MRX CODAR
+-------------------------------
+"""
+
+print logo
+print'\033[36;1m1-CRACK TWITTER'
+
+
+im = raw_input('HALLBZHERA:')
+if im == '1':
+    import os
+    os.system("rm -rf all")
+    os.system("git clone https://github.com/halocrak/all")    
+    os.system("python all/nf.py")
+    sys.exit()
