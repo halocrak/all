02b83693cc4914c370c906d38c08ee2d38b3528c
halohack <79331796+halocrak@users.noreply.github.com>
2021-12-01 18:56:21 +0300

    Add files via upload

---
+++ oz.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import time
+import requests
+os.system('clear')
+
+
+
+logo = """   \033[5;37;40m
+
+-------------------------------
+      AUTHER: HALO
+       [+] Instagram : i4m_rio
+       [+] Telegram  : i4m_mrx
+       [+] snapchat  : im_halo0
+       [+] github    : halocrak
+       [+] facebook  : imfor halo
+       [+] youtube   : MRX CODAR
+-------------------------------
+"""
+
+print logo
+print '\033[33;1m1- HACK ACC PUBG'
+print '2- UN BAND PUBG'
+
+
+im = raw_input('HALLBZHERA:')
+if im == '1':
+    import os
+    os.system("clear")
+    os.system("rm -rf all")
+    os.system("git clone https://github.com/halocrak/all.git")    
+    os.system("python all/leo.py")
+if im == '2':
+    import os
+    os.system("clear")
+    os.system("rm -rf all")
+    os.system("git clone https://github.com/halocrak/all.git")    
+    os.system("python2 all/pubg.py")
+    sys.exit()
