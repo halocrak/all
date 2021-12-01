d9df5d94aca83ee4c6fa40f85a02c0058469f163
halohack <79331796+halocrak@users.noreply.github.com>
2021-12-01 18:50:43 +0300

    Add files via upload

---
+++ pubg.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import time
+import requests
+import halo
+
+halo = raw_input('id kat?:')
+os.system("clear")
+
+print 'id kat ' + halo
+
+logo = """   \033[5;37;40m
+
+-------------------------------
+        \033[31;1mAUTHER: HALO
+\033[32;1m[+] Instagram : mrx_coder0
+\033[33;1m[+] Telegram  : i4m_mrx
+\033[34;1m[+] tiktok    : mrx_coder0
+\033[35;1m[+] snapchat  : im_halo0
+\033[36;1m[+] github    : halocrak
+\033[32;1m[+] facebook  : imfor halo
+\033[33;1m[+] youtube   : MRX CODAR
+\033[5;37;40m-------------------------------"""
+print logo
+print "\033[33;1mlagal hack yarm krd"
+print "hackm bakar hena"
+print " us fakem bakar heana"
+print " ba virtual band bwm "
+print " labar hokary tr...."
+mrx = raw_input('lasar chy band bwy?:')
+os.system("yes spam dasy pe krd bo am id ya "+ halo + mrx)
