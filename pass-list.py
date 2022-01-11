7e11984e97844504627b8c388ca309aa5fec19c6
halohack <79331796+halocrak@users.noreply.github.com>
2022-01-11 14:16:27 +0300

    Add files via upload

---
+++ pass-list.py
@@ -0,0 +1,46 @@
+import random
+import os
+import pyTextColor
+
+pytext = pyTextColor.pyTextColor()
+
+team = pytext.format_text(text="1877", color="#ff0000",
+                          bgcolor="#FFFF00")  
+
+print(team)  
+
+
+os.system("rm -rif 1877.txt")
+os.system("rm -rif nano")
+os.system('figlet 1877')
+halo = input("NAWEK BNWSA : ")
+b =("0750","0783","0751","0782","0771","0770","0772","0773","0780","0774","0781")
+g =("112233445566","112233445566","123456654321","1234512345","1234554321")
+mrx =("_",".")
+qq =("@yahoo.com","@gmail.com","@hotmali.com")
+op=open("/sdcard/Download/1877.txt","w")
+for x in range(10000):
+	f = "1234567890"
+	x1 = random.choice(f)
+	x2 = random.choice(f)
+	x3 = random.choice(f)
+	x4 = random.choice(f)
+	x5 = random.choice(f)
+	x6 = random.choice(f)
+	x7 = random.choice(f)
+	x8 = random.choice(f)
+	x9 = random.choice(f)
+	x10 = random.choice(f)
+	x11 = random.choice(f)
+	x12 = random.choice(f)
+	x13 = random.choice(f)
+	x14 = random.choice(f)
+	x15 = random.choice(f)
+	x16 = random.choice(f)
+	dd = halo+x1+x3+x4
+	
+	kurd = dd
+	print(kurd)
+	op.write(kurd+"\n")
+	
+	
