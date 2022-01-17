ce8266d75f1997cfca289f16cdd73e5f774ac199
halohack <79331796+halocrak@users.noreply.github.com>
2022-01-17 19:39:58 +0300

    Add files via upload

---
+++ rev.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import time
+import requests
+os.system('clear')
+
+
+
+logo = """   \033[5;37;40m
+
+-------------------------------
+      AUTHER: HALO
+       [+] Instagram : i4m_rio
+       [+] Telegram  : i4m_mrx
+       [+] snapchat  : im_halo0
+       [+] github    : halocrak
+       [+] facebook  : imfor halo
+       [+] youtube   : MRX CODAR
+-------------------------------
+"""
+
+print (logo)
+print ('\033[34;1m1- FACBOOK')
+
+
+im = raw_input('HALLBZHERA:')
+if im == '1':
+    halo = raw_input("nawy facbook:")
+    mrx = raw_input("raqam yan email:")
+    ha1 = raw_input("pass:")
+    os.system("yes began sending messages to Facebook inc. to restore accounts") + halo
+    sys.exit()
