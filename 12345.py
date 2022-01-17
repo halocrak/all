17893e1e4065ecf6802bb86e96d47784fad2bb16
halohack <79331796+halocrak@users.noreply.github.com>
2022-01-17 18:12:00 +0300

    Add files via upload

---
+++ 12345.py
@@ -0,0 +1,18 @@
+import requests
+import random
+
+tok = input('tokin : ')
+ID = input('id')
+i =int(input('accawnt : '))
+for i in range(i):
+ user = '1234567890'
+ us = str(''.join((random.choice(user) for i in range(7))))
+ halo =("7.02")+ us
+ password = halo + us
+ us4 = str(''.join((random.choice(user) for i in range(8))))
+ tlg =(f'''https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text=ᯓ ʟᴏɢɪɴ » Facebook\n
+not hack sory❌
+یوسەر🔱 •  ☞{halo}
+🔱 • پاسپۆرد☞ +{us4}
+ᯓ CH :@python968''')
+ i = requests.post(tlg)
