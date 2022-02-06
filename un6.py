b43c3faae80c1d30896b06779576453d6f345ac4
halohack <79331796+halocrak@users.noreply.github.com>
2022-02-06 12:48:41 +0300

    Add files via upload

---
+++ un6.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import time
+import requests
+import time,sys
+os.system('clear')
+logo = """
+\x1b[1;92m
+ __    __       ___       __        ______   
+|  |  |  |     /   \     |  |      /  __  \  
+|  |__|  |    /  ^  \    |  |     |  |  |  | 
+|   __   |   /  /_\  \   |  |     |  |  |  | 
+|  |  |  |  /  _____  \  |  `----.|  `--'  | 
+|__|  |__| /__/     \__\ |_______| \______/  
+                                             
+
+                        
+"""
+print (logo)
+halo =raw_input("link site")
+time.sleep(5)
+print("\x1b[1;92m[FOUND] http://968.rf.gd/admin.html")
