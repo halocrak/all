d97c0d22de1a8b0a19d02febfa774ca9897fce97
halohack <79331796+halocrak@users.noreply.github.com>
2022-02-18 23:22:01 +0300

    Add files via upload

---
+++ dm.py
@@ -0,0 +1,26 @@
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, getpass
+import requests
+os.system("pkg install wget -y ;pip3 install wget ;pip2 install wget ;python2 -m ensurepip ;pip install —upgrade pip ;pip2 install requests mechanize ;pip2 install requests ;clear")
+BL4CK="     \n     ____    ____   __   __   ____   _   _    ___    \n    |  _ \  / ___|  \ \ / /  / ___| | | | |  / _ \    \n    | |_) | \___ \   \ V /  | |     | |_| | | | | |    \n    |  __/   ___) |   | |   | |___  |  _  | | |_| |    \n    |_|     |____/    |_|    \____| |_| |_|  \___/   "
+print("\033[96;1m")
+print(BL4CK)
+riz="\033[34m≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈"
+print(riz)
+print("\033[0;1m       \n    Auther-BL4CK D4Y\n    Telegram-@FR13NDS_CLAY\n    Instagram : ue3ex\n    My Github : https://github.com/BL4CK-d4y\n    My Chanel : KURD_HACK_APK\n    For Activing The Tool Dm Me\033[0;1m \033[31m@FR13NDS_CLAY\033[0;1m  \033[0;1m   \n    \033[31mPrice : 5$\033[0;1m ")
+print(riz)
+(2)
+os.system("xdg-open https://t.me/FR13NDS_CL4Y")
+ss="BL4CK"
+s="PSYCHO"
+print("\033[36;1m")
+sss=input("  USER : ")
+ssss=input("  PASS : ")
+if ss==sss and s==ssss:
+  print("\033[32m Tawawa :)\033[0;1m ✓\033[0;1m")
+  time.sleep(3)
+  os.system("rm -rf PSYCHO ;git clone https://github.com/fuckkyouuu/PSYCHO ;cd PSYCHO ;python3 PSYCHO.py")
+else:
+  import os
+  print("\033[31mhallaya :( !\033[0;1m")
+  time.sleep(3)
+  os.system("clear ;python PSYCHO.py")
